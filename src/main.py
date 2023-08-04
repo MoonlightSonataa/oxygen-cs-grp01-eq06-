3d3a6f58e9969a4c13d92e0463be1a3acea49e8d
MoonlightSonataa <46796396+MoonlightSonataa@users.noreply.github.com>
2023-08-03 23:30:46 -0400

    added function to add data dynamically to the database

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import logging
 import time
 import yaml
 import requests
+import psycopg2
 
 from signalrcore.hub_connection_builder import HubConnectionBuilder
 
@@ -25,6 +26,31 @@ class Main:
         print(f"TOKEN: '{self.token}'")
 
         self.validate_token()
+        
+        
+    def send_temperature_to_postgres(self, timestamp, temperature):
+        try:
+            conn = psycopg2.connect(
+                host="35.192.151.166",  # your database host
+                database="LOG680_DEVOPS",  # your database name
+                user="postgres",  # your database username
+                password="postgres"  # your database password
+            )
+            cur = conn.cursor()
+
+            query = """
+            INSERT INTO temperature_logs (log_time, temperature) 
+            VALUES (%s, %s)
+            """
+            cur.execute(query, (timestamp, temperature))
+
+            conn.commit()
+            cur.close()
+            conn.close()
+
+            print("Temperature data sent to DB successfully.")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(f"Error sending temperature data to DB: {error}")
 
     def __del__(self):
         if self._hub_connection is not None:
@@ -69,10 +95,11 @@ class Main:
             print(data[0]["date"] + " --> " + data[0]["data"])
             date = data[0]["date"]
             date_dataa = float(data[0]["data"])
-            self.send_temperature_to_fastapi(date, date_dataa)
+            self.send_temperature_to_postgres(date, date_dataa)  # replace this line
             self.analyze_datapoint(date, date_dataa)
         except IndexError as err:
             print(err)
+
 
     def analyze_datapoint(self, date, data):
         if float(data) >= float(self.t_max):
@@ -88,22 +115,36 @@ class Main:
         details = json.loads(request.text)
         print(str(details) + " date" + date)
 
-    def send_temperature_to_fastapi(self, timestamp, temperature):
+
+
+    def send_temperature_to_db(self, timestamp, temperature):
         try:
-            payload = {"timestamp": timestamp, "temperature": temperature}
-            response = requests.post(
-                f"{self.host}/api/temperature",
-                json=payload,
-                timeout=5,
+            # Connect to your postgres DB
+            conn = psycopg2.connect(
+                dbname="LOG680_DEVOPS",
+                user="postgres",
+                password="postgres",
+                host="35.192.151.166",
             )
-            if response.status_code == 200:
-                print("Temperature data sent to FastAPI successfully.")
-            else:
-                print(
-                    f"Failed to send temperature data. Response code: {response.status_code}"
-                )
-        except requests.RequestException as err:
-            print(f"Error sending temperature data to FastAPI: {err}")
+
+            # Open a cursor to perform database operations
+            cur = conn.cursor()
+
+            # Execute a query
+            cur.execute("INSERT INTO temperature_logs (log_time, temperature) VALUES (%s, %s)", (timestamp, temperature))
+
+            # Commit the transaction
+            conn.commit()
+
+            # Close the cursor and connection
+            cur.close()
+            conn.close()
+
+            print("Temperature data sent to DB successfully.")
+
+        except Exception as e:
+            print(f"Failed to send temperature data to DB. Error: {str(e)}")
+
 
     def validate_token(self):
         """
